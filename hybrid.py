fad679185b8a1e2603f77e71e5e4ede19414e589
Vedant Agarwal <vedantagarwal4@gmail.com>
2022-02-11 16:16:49 +0530

    steganographic implementation for the cipher

--- hybrid.py
+++ hybrid.py
@@ -1,4 +1,127 @@
+# importing the required libraries  
+import cv2  
+import os
+import numpy as np  
+import types   
 from string import ascii_uppercase as alphabet
+
+path = os.getcwd()
+# converting types to binary  
+def msg_to_bin(msg):  
+    if type(msg) == str:  
+        return ''.join([format(ord(i), "08b") for i in msg])  
+    elif type(msg) == bytes or type(msg) == np.ndarray:  
+        return [format(i, "08b") for i in msg]  
+    elif type(msg) == int or type(msg) == np.uint8:  
+        return format(msg, "08b")  
+    else:  
+        raise TypeError("Input type not supported")  
+
+  
+# defining function to hide the secret message into the image  
+def hide_data(img, secret_msg):  
+    # calculating the maximum bytes for encoding  
+    nBytes = img.shape[0] * img.shape[1] * 3 // 8  
+    #print("Maximum Bytes for encoding:", nBytes)  
+    # checking whether the number of bytes for encoding is less  
+    # than the maximum bytes in the image  
+    if len(secret_msg) > nBytes:  
+        raise ValueError("Error encountered insufficient bytes, need bigger image or less data!!")  
+    secret_msg += '#####'       # we can utilize any string as the delimiter  
+    dataIndex = 0  
+    # converting the input data to binary format using the msg_to_bin() function  
+    bin_secret_msg = msg_to_bin(secret_msg)  
+    #print("bin_secret_msg ********** : ", bin_secret_msg)
+    # finding the length of data that requires to be hidden  
+    dataLen = len(bin_secret_msg)  
+    #print("length of bin_secret_msg : ", bin_secret_msg)
+    for values in img:  
+        for pixels in values:  
+            # converting RGB values to binary format  
+            r, g, b = msg_to_bin(pixels)  
+            # modifying the LSB only if there is data remaining to store  
+            if dataIndex < dataLen:  
+                # hiding the data into LSB of Red pixel  
+                pixels[0] = int(r[:-1] + bin_secret_msg[dataIndex], 2)  
+                dataIndex += 1  
+            if dataIndex < dataLen:  
+                # hiding the data into LSB of Green pixel  
+                pixels[1] = int(g[:-1] + bin_secret_msg[dataIndex], 2)  
+                dataIndex += 1  
+            if dataIndex < dataLen:  
+                # hiding the data into LSB of Blue pixel  
+                pixels[2] = int(b[:-1] + bin_secret_msg[dataIndex], 2)  
+                dataIndex += 1  
+            # if data is encoded, break out the loop  
+            if dataIndex >= dataLen:  
+                break  
+      
+    return img
+
+def show_data(img):  
+    bin_data = ""  
+    for values in img:  
+        for pixels in values:  
+            # converting the Red, Green, Blue values into binary format  
+            r, g, b = msg_to_bin(pixels)  
+            # data extraction from the LSB of Red pixel  
+            bin_data += r[-1]  
+            # data extraction from the LSB of Green pixel  
+            bin_data += g[-1]  
+            # data extraction from the LSB of Blue pixel  
+            bin_data += b[-1]  
+    # split by 8-Bits  
+    allBytes = [bin_data[i: i + 8] for i in range(0, len(bin_data), 8)]  
+    # converting from bits to characters  
+    decodedData = ""  
+    for bytes in allBytes:  
+        decodedData += chr(int(bytes, 2))  
+        # checking if we have reached the delimiter which is "#####"  
+        if decodedData[-5:] == "#####":  
+            break  
+    # print(decodedData)  
+    # removing the delimiter to display the actual hidden message  
+    return decodedData[:-5]  
+
+# defining function to encode data into Image  
+def encodeText(data):  
+    img_name = input("Enter name of the image (with extension): ")
+    img_name = os.path.join(path,img_name)  
+    # reading the input image using OpenCV-Python  
+    img = cv2.imread(img_name)  
+    # printing the details of the image    
+    data = data
+    if (len(data) == 0):  
+        raise ValueError('Data is Empty')  
+      
+    file_name = input("Enter name of the new encoded image (with extension): ") 
+    file_name = os.path.join(path,file_name)
+    # calling the hide_data() function to hide the secret message into the selected image  
+    encodedImage = hide_data(img, data)  
+    cv2.imwrite(file_name, encodedImage) 
+    print("The shape of the image is: ", img.shape) # checking the image shape to calculate the number of bytes in it  
+    #print("The original image is as shown below: ")  
+    # resizing the image as per the need  
+    resizedImg = cv2.resize(img, (500, 500))  
+    # displaying the image  
+    cv2.imshow('',resizedImg)
+    cv2.waitKey(0) 
+  
+# defining the function to decode the data in the image  
+def decodeText():  
+    # reading the image containing the hidden image  
+    img_name = input("Enter the name of the Steganographic image that has to be decoded (with extension): ")  
+    img = cv2.imread(img_name)  # reading the image using the imread() function  
+  
+    key = str(input("[+] Enter your key - "))
+    print("The Steganographic image is as follow: ")  
+    resizedImg = cv2.resize(img, (500, 500))    # resizing the actual image as per the needs  
+    cv2.imshow('',resizedImg)  # displaying the Steganographic image  
+    cv2.waitKey(0) 
+    text = show_data(img)  
+    return text
+
+
 
 
 # This function generates the 
@@ -36,9 +159,9 @@ def originalText(cipher_text, key):
         x += ord('A') 
         orig_text.append(chr(x)) 
     return("" . join(orig_text)) 
-    
+
  
- 
+
 
 
 
@@ -87,26 +210,28 @@ def decoding(text, key):
 
 
 def assembly(mode):
-    text = str(input("[+] Enter your plain text - "))
     if mode == 0:
+        text = str(input("[+] Enter your plain text - "))
+        text_upper = text.upper()
         keyword = str(input("[+] Enter your key - "))
-        string = text
+        keyword = keyword.upper()
+        string = text_upper
         key = generateKey(string, keyword) # The newly generated autokey is stored in the key variable. 
-        print("Plain Text : " , string)
+        print("String : " , string)
         print("Key : ", key) 
         cipher_text = cipherText(string,key) 
         finished_text = encoding(cipher_text)
         print("After Vignere Cipher : ", cipher_text)
 
-        print("Encoded Text via Hybrid Application of Vignere and Polybius Square Cipher : ", finished_text)
+        print("\n »» Encoded Text via Hybrid Application of Vignere and Polybius Square Cipher : ««")
+        print(finished_text)
         print()
+        encodeText(string)
+        print("The given image has been Encoded.") 
 
     else:
-        key = str(input("[+] Enter your key - "))
-        finished_text = decoding(text, key)
-        print("Decoded Text via Hybrid Application of Vignere and Polybius Square Cipher :", finished_text)
+        print("Decoded message is : " + decodeText())
 
-    
 
 
 def main():
